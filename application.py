947d1d972ebbe9cea240af93be694a1398e35e7c
Julia Herron <julia.herron@locusenergy.com>
2016-11-06 10:18:05 -0800

    add blueprint and first route

--- application.py
+++ application.py
@@ -1,11 +1,33 @@
-from flask import Flask
+from flask import Blueprint, Flask
+from models import Puppy
+from schema import puppy_schema, puppies_schema
+from utils import jsonapi
+
+
+# Create a blueprint for puppy CRUD actions
+puppies_v1 = Blueprint('puppies_v1', __name__)
+
+
+@puppies_v1.route("/puppies")
+def get_all_puppies():
+    puppies = Puppy.query.all()
+    data, _ = puppy_schema.dump(puppies)
+    return jsonapi(data)
 
 
 def create_application():
     app = Flask('kiosk_backend')
+    app.register_blueprint(puppies_v1)
+    app.config.update(
+        SQLALCHEMY_DATABASE_URI="sqlite:///sqlalchemy_example.db",
+        SQLALCHEMY_TRACK_MODIFICATIONS=False,
+        DEBUG=True
+    )
+
     # keep application-specific state separate from SQLALchemy extension object
-    # from models import db
-    # db.init_app(app)
+    from models import db
+    db.init_app(app)
+
     return app
 
 
